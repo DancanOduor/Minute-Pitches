dc4c8603ad67049caa28ea3d7cf779d31ab5e333
Dancanoduor <dancanoduor1990@gmail>
2019-11-26 16:38:30 +0300

    Add user loginfuction

--- app/auth/views.py
+++ app/auth/views.py
@@ -10,8 +10,8 @@ from ..email import mail_message
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        ser = User.query.filter_by(email = login_form.email.data).first()
-        if user is not None and user.verify_password(login_form.password.data):
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if User is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
 
