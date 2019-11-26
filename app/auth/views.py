30fff628cdf790cd5200cd9547b6bd49027edb11
Dancanoduor <dancanoduor1990@gmail>
2019-11-26 14:32:19 +0300

    Add class role & uninstall pylint

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from flask_login import login_user,logout_user,login_required
 from . import auth
 from ..models import User
 from .forms import LoginForm,RegistrationForm
-from .. import db
+from .. import db column
 from ..email import mail_message
 
 @auth.route('/login',methods=['GET','POST'])
--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
-from . import db
+from . import db, Column
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -96,3 +96,11 @@ class Comment(db.Model):
     def get_comments(cls,pitch):
         comments = Comment.query.filter_by(pitch_id=pitch).all()
         return comments
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key= True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User', backref = 'role', lazy="dynamic")
+    def __repr__(self):
+        return f'User {self.name}'
