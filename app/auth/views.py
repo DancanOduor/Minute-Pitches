50d56a609dfd7a5719f19ad3297959c3585dcd1c
Dancanoduor <dancanoduor1990@gmail>
2019-11-27 15:29:06 +0300

    Add comment

--- app/auth/views.py
+++ app/auth/views.py
@@ -7,21 +7,21 @@ from .. import db
 from ..email import mail_message
 
 
-@auth.route('/signup',methods=['GET','POST'])
-def signup():
-    form = SignUpForm()
-    if form.validate_on_submit():
-        user =  User(email = form.email.data, username = form.username.data,password = form.password.data)
-        user.save_user()
-        try:
-            msg = Message('Hello...Welcome to pitches.We are glad you joined us',sender=('Dancansteric@gmail.com'))
-            msg.add_recipient(user.email)
-            mail.send(msg)
-        except Exception as e:
-            print('failed')
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
+# @auth.route('/signup',methods=['GET','POST'])
+# def signup():
+#     form = SignUpForm()
+#     if form.validate_on_submit():
+#         user =  User(email = form.email.data, username = form.username.data,password = form.password.data)
+#         user.save_user()
+#         try:
+#             msg = Message('Hello...Welcome to pitches.We are glad you joined us',sender=('Dancansteric@gmail.com'))
+#             msg.add_recipient(user.email)
+#             mail.send(msg)
+#         except Exception as e:
+#             print('failed')
+#         return redirect(url_for('auth.login'))
+#         title = "New Account"
+#     return render_template('auth/register.html',registration_form = form)
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
